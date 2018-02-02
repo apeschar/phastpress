a6c95731a5a86fff70a3eb5d585405d8f50a31fc
Milko Kosturkov <milko@kiboit.com>
2018-02-02 15:47:42 +0100

    Improve low PHP version notice

--- templates/low-php-version.php
+++ templates/low-php-version.php
@@ -6,9 +6,9 @@
     <div class="phastpress-settings-problem phastpress-settings-error">
         <?php echo sprintf(
                 __(
-                    'The version of PHP your server is running is lower than the required by PhastPress!<br>' .
-                    'You will not be able to run PhasPress unless you upgrade.<br>' .
-                    'Required PHP version >= 5.6. Your PHP version = %s',
+                    'The version of PHP your server is running is lower than that required by PhastPress!<br>' .
+                    'You will not be able to run PhastPress unless you upgrade to PHP 5.6 or higher.<br>' .
+                    'Required PHP version: >= 5.6. Your PHP version: %s.',
                     'phastpress'
                 ),
                 PHP_VERSION
