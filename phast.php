746ba623a86ff0d2d9472e3d44dacb49392449c0
Milko Kosturkov <milko@kiboit.com>
2018-05-03 20:41:19 +0300

    Set the certificates to use for Requests lib WIP #17

--- phast.php
+++ phast.php
@@ -6,5 +6,6 @@ require_once __DIR__ . '/functions/service.php';
     $service_config = phastpress_get_service_config();
     require_once $service_config['wp_includes_dir'] . '/class-requests.php';
     \Requests::register_autoloader();
+    \Requests::set_certificate_path($service_config['wp_includes_dir'] . '/certificates/ca-bundle.crt');
     return phastpress_get_service_config();
 });
