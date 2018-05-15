9880579b2d53a11d79b897f7cee48ac7cb02275f
Milko Kosturkov <milko@kiboit.com>
2018-05-15 16:54:34 +0300

    Improve Information

--- admin-front-end/src/components/Information.vue
+++ admin-front-end/src/components/Information.vue
@@ -21,46 +21,57 @@ export default {
 
 <style scoped lang="sass">
 
+  $title-color: #82d6f8
+  $border-color: #f4f4f4
+  $content-color: #c2c2c2
+
+  @mixin exclamation($size, $color)
+    content: "\0021"
+    width: $size
+    height: $size
+    border: 1px solid $color
+    border-radius: $size
+    text-align: center
+    color: $color
+    font-weight: bold
+
   .information
     display: inline-block
     position: relative
-    z-index: 1000
 
   .information:hover .block
     display: block
 
-  .icon
+  .icon::before
+    @include exclamation(12px, $title-color)
     display: inline-block
     margin-left: 5px
-    width: 12px
-    height: 12px
-    background: no-repeat center/12px url('../assets/exclamation.png')
-    opacity: 0.5
     cursor: pointer
 
   .block
     display: none
     position: absolute
-    top: -20px
+    top: -17px
     left: 14px
     margin-left: 15px
+    z-index: 1000
     box-sizing: border-box
     width: 329px
     padding: 12px 10px 15px 56px
-    border: 1px solid #f4f4f4
+    border: 1px solid $border-color
     border-radius: 4px
     background-color: white
+    box-shadow: 5px 12px 24px -8px rgba(0, 0, 0, .2)
+    font-size: 12px
+    color: $content-color
 
   .block::before
+    @include exclamation(24px, #858585)
     display: block
     position: absolute
-    top: 15px
+    top: 10px
     left: 20px
-    content: " "
-    width: 24px
-    height: 24px
-    background: no-repeat 0 0 url('../assets/exclamation.png')
-    opacity: 0.5
+    font-size: 24px
 
   .block::after
     display: block
@@ -71,19 +82,14 @@ export default {
     left: -9px
     content: " "
     transform: rotate(45deg)
-    border-left: 1px solid #f4f4f4
-    border-bottom: 1px solid #f4f4f4
+    border-left: 1px solid $border-color
+    border-bottom: 1px solid $border-color
     background: white
 
   .title
-    margin-bottom: 8px
-    font-size: 12px
+    margin-bottom: 5px
     font-weight: bold
-    color: #74d2f6
+    color: $title-color
     text-transform: uppercase
-
-  .content
-    font-size: 12px
-    color: #c2c2c2
 
 </style>
