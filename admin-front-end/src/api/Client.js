84fc2382c4f6b84cbbad939e897af330b91709bc
Milko Kosturkov <milko@kiboit.com>
2018-05-23 12:44:16 +0300

    Do no allow concurrent requests for save from one tab

--- admin-front-end/src/api/Client.js
+++ admin-front-end/src/api/Client.js
@@ -1,10 +1,48 @@
 import axios from 'axios'
 
+class RequestsAggregator {
+  constructor (makeRequestCb) {
+    this._makeRequestCb = makeRequestCb
+    this._lastRequestParams = null
+    this._resolvers = []
+    this._active = false
+  }
+
+  execute (requestParams) {
+    this._lastRequestParams = requestParams
+    const p = new Promise((resolve, reject) => this._resolvers.push({resolve, reject}))
+    if (!this._active) {
+      this._active = true
+      this._doRequest()
+    }
+    return p
+  }
+
+  async _doRequest () {
+    const params = this._lastRequestParams
+    const resolvers = this._resolvers
+    this._lastRequestParams = null
+    this._resolvers = []
+    try {
+      const response = await this._makeRequestCb(params)
+      resolvers.forEach(({resolve}) => resolve(response.data))
+    } catch (e) {
+      resolvers.forEach(({reject}) => reject(e))
+    }
+    if (this._lastRequestParams) {
+      setTimeout(this._doRequest.bind(this))
+    } else {
+      this._active = false
+    }
+  }
+}
+
 export default class WordPressAPIClient {
   constructor (adminUrl) {
     this._adminUrl = adminUrl
     this._nonceName = null
     this._nonce = null
+    this._saveAggregator = new RequestsAggregator(this._doConfigSave.bind(this))
   }
 
   getAdminPanelData () {
@@ -19,7 +57,10 @@ export default class WordPressAPIClient {
   }
 
   saveConfig (config) {
-    // TODO: Add some debouncing here
+    return this._saveAggregator.execute(config)
+  }
+
+  _doConfigSave (config) {
     const data = new FormData()
     data.set('action', 'phastpress_save_config')
     data.set(this._nonceName, this._nonce)
@@ -27,6 +68,5 @@ export default class WordPressAPIClient {
       data.set('phastpress-' + key, config[key] ? 'on' : 'off')
     })
     return axios.post(this._adminUrl, data)
-      .then(response => response.data)
   }
 }
