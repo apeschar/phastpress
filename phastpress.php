23540f3c1b1f92b104e673b5a33b378d9ac1346e
Albert Peschar <albert@peschar.net>
2020-03-27 15:14:25 +0200

    Release PhastPress 1.42

--- phastpress.php
+++ phastpress.php
@@ -2,13 +2,13 @@
 /*
 Plugin Name: PhastPress
 Description: PhastPress automatically optimizes your site for the best possible Google PageSpeed Insights score.
-Version: 1.41
+Version: 1.42
 Author: Albert Peschar
 Author URI: https://kiboit.com
 License: AGPLv3
 */
 
-define('PHASTPRESS_VERSION', '1.41');
+define('PHASTPRESS_VERSION', '1.42');
 define('PHASTPRESS_PLUGIN_FILE', __FILE__);
 
 if (version_compare(PHP_VERSION, '5.6.20', '<')) {
